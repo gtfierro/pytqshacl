9b4ef100bdbb999a8ad033b59fe144cd07451cc1
Majid alDosari <majid.aldosari@pnnl.gov>
2025-01-05 15:29:51 -0500

    no need for init here. they get init in run.py

--- src/pytqshacl/__init__.py
+++ src/pytqshacl/__init__.py
@@ -1,7 +1,4 @@
-__version__ = "113"
+__version__ = "114"
 from .topquadrant import ver as tq_ver
 topquadrant_version = tq_ver
-from .topquadrant.install import Java, Shacl
-Java()
-Shacl()
 from .run import validate, infer

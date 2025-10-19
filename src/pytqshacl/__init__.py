6bec138aa86d91f6d35694adf19c5fc375859464
Gabe Fierro <gtfierro@mines.edu>
2025-10-19 15:51:38 -0600

    more changes

--- src/pytqshacl/__init__.py
+++ src/pytqshacl/__init__.py
@@ -2,3 +2,8 @@ __version__ = "141"
 from .topquadrant import ver as tq_ver
 topquadrant_version = tq_ver
 from .run import validate, infer
+
+
+def cli(argv=None):
+    from .cli import cli as _cli
+    return _cli(argv)
